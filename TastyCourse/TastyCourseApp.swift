7746cd6a9393a63a9aa4dc48ca8dbffeac76899d
Konstantin Antonov <kostya-ant@yandex.ru>
2023-06-10 13:18:28 +0400

    Import FireBase SDK

--- TastyCourse/TastyCourseApp.swift
+++ TastyCourse/TastyCourseApp.swift
@@ -6,14 +6,29 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+
 
 let screen = UIScreen.main.bounds
 
 @main
 struct TastyCourseApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             AuthView()
         }
     }
+    
+    
+    
 }
