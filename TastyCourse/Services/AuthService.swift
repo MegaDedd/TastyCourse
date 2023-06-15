4bbb809d78cdc38d1d0948c13d5d4f28a00f9ef1
Konstantin Antonov <kostya-ant@yandex.ru>
2023-06-15 12:41:02 +0400

    succses(writing the user to the firebase collection)

--- TastyCourse/Services/AuthService.swift
+++ TastyCourse/Services/AuthService.swift
@@ -20,7 +20,17 @@ class AuthService {
     func signUp(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
         auth.createUser(withEmail: email, password: password) { result, error in
             if let result = result {
-                completion(.success(result.user))
+                
+                let userModel = MWUser(id: result.user.uid, name: "", phone: 0, address: "")
+                
+                DataBaseService.shared.setProfile(user: userModel) { resultDB in
+                    switch resultDB {
+                    case .success(_):
+                        completion(.success(result.user))
+                    case .failure(let error):
+                        completion(.failure(error))
+                    }
+                }
             } else if let error = error {
                 completion(.failure(error))
             }
