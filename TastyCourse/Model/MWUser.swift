59e79e1581b68c2dfed7b1c85779cc9f975c29f0
Konstantin Antonov <kostya-ant@yandex.ru>
2023-06-15 12:44:16 +0400

    create model MWUser

---
+++ TastyCourse/Model/MWUser.swift
@@ -0,0 +1,24 @@
+//
+//  UserModel.swift
+//  TastyCourse
+//
+//  Created by Константин on 12.06.2023.
+//
+
+import Foundation
+
+struct MWUser: Identifiable {
+    var id: String
+    var name: String
+    var phone: Int
+    var address: String
+    
+    var representation: [String : Any] {
+        var repres = [String : Any]()
+        repres["id"] = self.id
+        repres["name"] = self.name
+        repres["phone"] = self.phone
+        repres["address"] = self.address
+        return repres
+    }
+}
