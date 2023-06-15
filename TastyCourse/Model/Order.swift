ae93b3eef1d92fe6b0a2e515a173e8fd8cf5679d
Konstantin Antonov <kostya-ant@yandex.ru>
2023-06-15 14:43:10 +0400

    create model: Order

--- TastyCourse/Model/Order.swift
+++ TastyCourse/Model/Order.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import FirebaseFirestore //
+
+struct Order {
+    var id: String = UUID().uuidString
+    var userID: String
+    var positions = [Position]()
+    var date: Date
+    var status: String
+    
+    var cost: Int {
+        var sum = 0
+        for pos in positions {
+            sum += pos.cost
+        }
+        return sum
+    }
+    var representation: [String : Any] {
+        var repres = [String : Any]()
+        
+        repres["id"] = id
+        repres["userID"] = userID
+        repres["date"] = Timestamp(date: date)
+        repres["status"] = status
+        return repres
+    }
+}
