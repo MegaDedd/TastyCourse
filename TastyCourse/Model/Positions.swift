b91f43bf77a086adfa0f80562ca9dc9b4a1acab6
Konstantin Antonov <kostya-ant@yandex.ru>
2023-06-15 14:44:39 +0400

    aded property representation for database

--- TastyCourse/Model/Positions.swift
+++ TastyCourse/Model/Positions.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-
+// позиция которая отправляется в базу данных
 struct Position: Identifiable {
     var id: String
     var product: Product
@@ -14,5 +14,17 @@ struct Position: Identifiable {
     
     var cost: Int {
         return product.price * self.count
+    }
+    var representation: [String : Any] {
+        
+        var repres = [String : Any]()
+        
+        repres["id"] = id
+        repres["count"] = count
+        repres["title"] = product.title
+        repres["price"] = product.price
+        repres["cost"] = self.cost
+
+        return repres
     }
 }
