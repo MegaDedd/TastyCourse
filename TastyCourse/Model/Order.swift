08bb94670905a4c5b0187582787b2ca2f92295f0
Konstantin Antonov <kostya-ant@yandex.ru>
2023-06-15 14:40:30 +0400

    create model:Order and staus order

---
+++ TastyCourse/Model/Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  TastyCourse
+//
+//  Created by Константин on 15.06.2023.
+//
+
+import Foundation
---
+++ TastyCourse/Model/OrderStatus.swift
@@ -0,0 +1,8 @@
+//
+//  OrderStatus.swift
+//  TastyCourse
+//
+//  Created by Константин on 15.06.2023.
+//
+
+import Foundation
